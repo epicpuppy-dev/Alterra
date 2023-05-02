f6d7b49e957e9699f617bcccd60f0b573931c471
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2023-05-02 13:59:33 -0700

    Added Pen and Screwdriver

---
+++ Items/Pen.cs
@@ -0,0 +1,31 @@
+﻿
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Alterra.Items
+{
+    internal class Pen : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 48;
+            Item.height = 48;
+            Item.value = Item.buyPrice(silver: 50);
+            Item.maxStack = 999;
+            Item.ResearchUnlockCount = 25;
+            Item.rare = ItemRarityID.Green;
+        }
+    }
+
+    public class PenNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Merchant)
+            {
+                shop.Add<Pen>();
+            }
+        }
+    }
+}
---
+++ Items/Screwdriver.cs
@@ -0,0 +1,33 @@
+﻿
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Alterra.Items
+{
+    internal class Screwdriver : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 40;
+            Item.height = 40;
+            Item.value = Item.buyPrice(gold: 10);
+            Item.maxStack = 999;
+            Item.ResearchUnlockCount = 25;
+            Item.rare = ItemRarityID.Green;
+        }
+    }
+
+    public class ScrewdriverNPC : GlobalNPC
+    {
+        public override void ModifyShop(NPCShop shop)
+        {
+            if (shop.NpcType == NPCID.Merchant)
+            {
+                shop.Add<Screwdriver>(new Condition("skeletronDefeated", () => NPC.downedBoss3));
+            }
+        }
+    }
+}
